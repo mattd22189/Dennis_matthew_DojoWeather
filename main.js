18dcc75225fbc914fa9c16e1809ad80d6eba583f
Matthew Dennis <59185749+mattd22189@users.noreply.github.com>
2023-06-23 16:57:12 -0400

    changed converter

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ and if the value of the selection is equal to the value of the variable
 I want the document to access all the numbers associated with the celsius
 selection and convert them to Fahrenheit. using the proper equation */
 
-let temperature1 = document.getElementById("temp").value
 
 function converter(){
+    let temperature1 = document.getElementById("temp").value
+    console.log(temperature1);
             if (temperature1 == "C"){
-        (document.querySelector(" .today1").innerText) = Math.round( (((document.querySelector(" .today1 ").innerText) * 1) - 32) * 5/9)
-    } else if (temperature1 == "F") { 
-        (document.querySelector(" .today1 ").innerText) = Math.round(( ((document.querySelector(" .today1").innerText) * 1) * 5/9) + 32)
+        console.log("converting to celsius")
+        document.querySelector(".today1").innerText = Math.round( (((document.querySelector(".today1").innerText) * 1) - 32) * 5/9)
+        document.querySelector(".today2").innerText = Math.round((((document.querySelector(".today2").innerText) * 1) - 32) * 5/9)       
+        document.querySelector(".tomorrow1").innerText = Math.round((((document.querySelector(".tomorrow1").innerText) * 1) - 32) * 5/9)
+        document.querySelector(".tomorrow2").innerText = Math.round( (((document.querySelector(".tomorrow2").innerText) * 1) - 32) * 5/9)
+        document.querySelector(".friday1").innerText = Math.round( (((document.querySelector(".friday1").innerText) * 1) - 32) * 5/9)
+        document.querySelector(".friday2").innerText = Math.round((((document.querySelector(".friday2").innerText) * 1) - 32) * 5/9)
+        document.querySelector(".saturday1").innerText = Math.round((((document.querySelector(".saturday1").innerText) * 1) - 32) * 5/9)
+        document.querySelector(".saturday2").innerText = Math.round((((document.querySelector(".saturday2").innerText) * 1) - 32) * 5/9)
+    } else { 
+       console.log("converting to f")
+        document.querySelector(" .today1 ").innerText = Math.round(( ((document.querySelector(" .today1").innerText) * 1) * 9/5) + 32)
     }
 }
 
